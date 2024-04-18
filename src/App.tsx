0f60f18f6cd8d3dae5c79993d2150f0968364c77
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2024-04-18 21:03:40 +0300

    Added route for Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/home/home.component';
 
-function App() {
+const App = () => {
   return (
-    <div></div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
